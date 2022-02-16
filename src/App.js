6f3c6a1c1f4202e487e5ffd12fc82f384a468a89
RavinduSathsara <rsathsara330@gmail.com>
2022-02-16 14:59:41 +0530

    Fetch data and show popular movies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
+
+const baseurl =
+  "https://api.themoviedb.org/3/movie/popular?api_key=&language=en-US&page=1";
 
 function App() {
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+    const data = await fetch(baseurl);
+    const movies = await data.json();
+    console.log(movies.results);
+    setPopular(movies.results);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="popular-movies">
+        {popular.map((movie) => {
+          return <Movie key={movie.id} movie={movie} />;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/Movie.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Movie = ({ movie }) => {
+  return (
+    <div>
+      <h2>{movie.title}</h2>
+      <img
+        src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path}
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Movie;
